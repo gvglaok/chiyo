99a5e722d171d89c45f8a933ee16bdc0403ca4dd
Keven <zhao.k.f@qq.com>
2016-06-08 15:45:48 +0800

    :cat: login ok

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -27,37 +27,30 @@
 </head>
 
 <body id="login">
-    <div class="login-logo">
-        <a href="index.html"><img src="<?php echo base_url(); ?>skin/admin/images/logo.png" alt="gvgnn.com" /></a>
+    <!-- <div class="login-logo">
+        <a href="#"><img src="<?php echo base_url(); ?>skin/admin/images/logo.png" alt="gvgnn.com" /></a>
     </div>
-    <h2 class="form-heading">易食代登录</h2>
+    <h2 class="form-heading">易食代登录</h2> -->
     <div class="app-cam">
+        <h3><img height="25px" src="<?php echo base_url(); ?>skin/admin/images/logo.png" alt="gvgnn.com" /> 易食代登录</h3>
+        <br>
         <form method="POST" role="form">
             <div class="input-group">
-                <span class="input-group-addon">用户名</span>
-                <input id="user" name="user" type="text" class="form-control" required="true">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i> </span>
+                <input id="user" name="user" type="text" placeholder="用户名" class="form-control" required="true">
             </div>
             <br>
             <div class="input-group">
-                <span class="input-group-addon">密&nbsp;&nbsp;码</span>
-                <input id="psw" name="psw" type="password" class="form-control" required="true">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i> </span>
+                <input id="psw" name="psw" type="password" placeholder="密  码" class="form-control" required="true">
             </div>
             <br>
             <button type="button" onclick="login()" class="btn btn-info btn-lg btn-block">登录</button>
         </form>
-        <br><br><br><br><br><br><br>
+        <br><br><br><p>Copyright &copy; 2016 CIFANF_YSD Company</p><br><br>
     </div>
     <div class="copy_layout login">
-        <p>Copyright &copy; 2016 CIFANF_YSD Company</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        
     </div>
 </body>
 
